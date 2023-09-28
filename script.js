0e884bae0269a756df37fbd5323b4c107f841ecb
SergioTales1 <algumemailai3@gmail.com>
2023-09-28 12:38:54 -0300

    lang button added

--- script.js
+++ script.js
@@ -102,3 +102,54 @@ const updateModal = function () {
 
 overlay.addEventListener('click', updateModal);
 closeModalEl.addEventListener('click', updateModal);
+
+// ------------------------ EN - PT --------------------------
+const currentTitle = document.querySelectorAll('.current-title');
+const enEl = document.getElementById('en');
+const ptEl = document.getElementById('pt');
+
+enEl.addEventListener('click', () => {
+  enEl.classList.add('active');
+  ptEl.classList.remove('active');
+  btnNewGame.textContent = '🔄 new game';
+  btnHold.textContent = '📥Hold';
+  btnRoll.textContent = '🎲roll dice';
+  player0El.children[0].textContent = 'PLAYER 1';
+  player1El.children[0].textContent = 'PLAYER 2';
+  for (let i = 0; i < currentTitle.length; i++) {
+    currentTitle[i].textContent = 'CURRENT';
+  }
+  // MODAL
+  modal.children[0].innerHTML = 'How to <span class="lightgreen">play</span> this game ?';
+  modal.children[1].innerHTML = `<span class="darkGray">1</span>- Click <span class="darkRed">ROLL DICE</span> to get 2 to 6
+  points, these points will be store into your <span class="lightGray">Current Score</span>.`;
+  modal.children[2].innerHTML = `<span class="darkGray">2</span>- Use <span class="darkBlue">HOLD</span> to save all your
+  current points into your global score.`;
+  modal.children[3].innerHTML = `<span class="darkGray">3</span>- If you hold your points or
+  <span class="red">get</span> a<span class="red"> 1</span> then it's other player's round to
+  roll the dice.`;
+  modal.children[4].innerHTML = `<span class="darkGray">4</span>- First player to reach
+  <span class="darkBlue">100</span> points <span class="yellow">win the game</span>!`;
+});
+
+ptEl.addEventListener('click', () => {
+  modal.style.height = '405px';
+  enEl.classList.remove('active');
+  ptEl.classList.add('active');
+  btnNewGame.textContent = '🔄 Novo Jogo';
+  btnHold.textContent = '📥Segurar';
+  btnRoll.textContent = '🎲Jogar dado';
+  player0El.children[0].textContent = 'JOGADOR 1';
+  player1El.children[0].textContent = 'JOGADOR 2';
+  for (let i = 0; i < currentTitle.length; i++) {
+    currentTitle[i].textContent = 'ATUAL';
+  }
+  // MODAL
+  modal.children[0].innerHTML = 'Como <span class="lightgreen">jogar</span> esse jogo ?';
+  modal.children[1].innerHTML = `<span class="darkGray">1</span>- Clique <span class="darkRed">JOGAR DADO</span> para receber de 2 a 6 pontos, esses pontos serão armazenados em sua <span class="lightGray">Pontuação Atual</span>.`;
+  modal.children[2].innerHTML = `<span class="darkGray">2</span>- Use <span class="darkBlue">SEGURAR</span> para salvar todos os seus pontos dentro de sua pontuação global.`;
+  modal.children[3].innerHTML = `<span class="darkGray">3</span>- Caso você segure seus pontos ou 
+  <span class="red">receba</span> o dado de valor<span class="red"> 1</span>, a vez de jogar passa para o outro jogador.`;
+  modal.children[4].innerHTML = `<span class="darkGray">4</span>- Primeiro jogador a coletar
+  <span class="darkBlue">100</span> pontos <span class="yellow">ganha o jogo</span>!`;
+});
